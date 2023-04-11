0e0475d1a207ac002ccfd3735ffbdb228979e64d
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-04-10 23:20:52 -0300

    feat(context): stored state inside localStorage

--- eWallet/src/context/authProvider/index.tsx
+++ eWallet/src/context/authProvider/index.tsx
@@ -1,22 +1,32 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { IAuthProvider, IContext, IUser } from "./type";
-import { LoginRequest } from "./util";
+import { LoginRequest, getUserLocalStorage, setUserLocalStorage } from "./util";
 
 export const AuthContext = createContext<IContext>({} as IContext)
 
 export const AutheProvider = ({ children }: IAuthProvider) => {
   const [user, setUser] = useState<IUser | null>()
 
+  useEffect(() => {
+    const user = getUserLocalStorage()
+
+    if (user) {
+      setUser(user)
+    }
+  }, [])
+
   async function authenticate(email: string, password: string) {
     const response = await LoginRequest(email, password)
 
-    const payload = {token: response.token, email }
+    const payload = { token: response.token, email }
 
     setUser(payload);
+    setUserLocalStorage(payload);
   }
 
   function logout() {
     setUser(null);
+    setUserLocalStorage(null);
   }
 
   return (
--- eWallet/src/context/authProvider/util.ts
+++ eWallet/src/context/authProvider/util.ts
@@ -1,4 +1,22 @@
 import { Api } from "../../services/api/Api";
+import { IUser } from "./type";
+
+export function setUserLocalStorage (user: IUser | null) {
+  localStorage.setItem('u', JSON.stringify(user))
+}
+
+export function getUserLocalStorage () {
+  const json = localStorage.getItem('u')
+
+  if (!json) {
+    return null;
+  }
+
+  const user = JSON.parse(json)
+
+  return user ?? null;
+}
+
 
 export async function LoginRequest(email: string, password: string) {
   try {
