da5557e50947168710947e77001101037add8600
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-04-10 21:53:33 -0300

    feat: created connection to the api

--- eWallet/src/context/authProvider/index.tsx
+++ eWallet/src/context/authProvider/index.tsx
@@ -1,3 +1,22 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+import { IAuthProvider, IContext, IUser } from "./type";
 
-export const AuthContext = createContext<>() 
+export const AuthContext = createContext<IContext>({} as IContext)
+
+export const AutheProvider = ({ children}: IAuthProvider) => {
+   const [user, setUser] = useState<IUser | null>()
+
+   async function authenticate (email: string, password: string) {
+
+   }
+
+   function logout () {
+
+   }
+
+   return (
+    <AuthContext.Provider value={{...user, authenticate, logout }}>
+        {children}
+    </AuthContext.Provider>
+   )
+}
